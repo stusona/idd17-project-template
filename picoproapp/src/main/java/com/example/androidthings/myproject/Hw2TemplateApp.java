c8525dc9352f914d30ebb759774ac2a4881e7cb4
stusona <supadupastu@gmail.com>
2017-09-05 14:57:28 -0700

    initial Android Studio commit

--- picoproapp/src/main/java/com/example/androidthings/myproject/Hw2TemplateApp.java
+++ picoproapp/src/main/java/com/example/androidthings/myproject/Hw2TemplateApp.java
@@ -8,33 +8,68 @@ import com.google.android.things.pio.Gpio;
  */
 
 public class Hw2TemplateApp extends SimplePicoPro {
-    @Override
-    public void setup() {
-        //set two GPIOs to input
-        pinMode(GPIO_128,Gpio.DIRECTION_IN);
-        setEdgeTrigger(GPIO_128,Gpio.EDGE_BOTH);
+    Gpio button1 = GPIO_128;
+    Gpio button2 = GPIO_39;
+    Gpio button3 = GPIO_37;
+    Gpio button4 = GPIO_35;
+    Gpio button5 = GPIO_34;
+    Gpio button6 = GPIO_33;
 
-        pinMode(GPIO_39,Gpio.DIRECTION_IN);
-        setEdgeTrigger(GPIO_39,Gpio.EDGE_BOTH);
+    long buttonTimer;
+
+    boolean buttonPressed = LOW;
+    boolean button1State;
+    boolean button2State;
+    boolean button3State;
+    boolean button4State;
+    boolean button5State;
+    boolean button6State;
+
+    public void setup() {
+
+        //set GPIOs to input
+        pinMode(button1, Gpio.DIRECTION_IN);
+        pinMode(button2, Gpio.DIRECTION_IN);
+        pinMode(button3, Gpio.DIRECTION_IN);
+        pinMode(button4, Gpio.DIRECTION_IN);
+        pinMode(button5, Gpio.DIRECTION_IN);
+        pinMode(button6, Gpio.DIRECTION_IN);
+
+        // set interrupts
+        setEdgeTrigger(button1, Gpio.EDGE_BOTH);
+        setEdgeTrigger(button2, Gpio.EDGE_BOTH);
+        setEdgeTrigger(button3, Gpio.EDGE_BOTH);
+        setEdgeTrigger(button4, Gpio.EDGE_BOTH);
+        setEdgeTrigger(button5, Gpio.EDGE_BOTH);
+        setEdgeTrigger(button6, Gpio.EDGE_BOTH);
+
     }
 
     @Override
     public void loop() {
         //nothing to do here
+        if ((millis() - buttonTimer) >= 300) {
 
+            buttonPressed = LOW;
+        }
     }
 
     @Override
     void digitalEdgeEvent(Gpio pin, boolean value) {
+
+        if (buttonPressed == LOW) {
+            buttonTimer = millis();
+            buttonPressed = HIGH;
+        }
+
         println("digitalEdgeEvent"+pin+", "+value);
-        // when 128 goes from LOW to HIGH
-        // this is on button button release for pull-up resistors
-        if(pin==GPIO_128 && value==HIGH) {
-            printCharacterToScreen('a');
-        }
-        //when 39 goes from HIGH to HIGH
-        else if (pin==GPIO_39 && value==HIGH) {
-            printCharacterToScreen('b');
-        }
+
+        if (pin==button1) button1State = value;
+        if (pin==button2) button2State = value;
+        if (pin==button3) button3State = value;
+        if (pin==button4) button4State = value;
+        if (pin==button5) button5State = value;
+        if (pin==button6) button6State = value;
+
     }
 }
